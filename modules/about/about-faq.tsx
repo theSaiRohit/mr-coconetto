0bbafcc2d2c423566541eed3981e478f1e08a449
Sai Rohit <sairohit11.s@gmail.com>
2023-08-05 09:48:41 +0530

    about skeleton

--- modules/about/about-faq.tsx
+++ modules/about/about-faq.tsx
@@ -1,28 +1,36 @@
-import { useCallback, useRef } from "react";
+import { useCallback, useRef, useState } from "react";
 import { MdExpandLess, MdExpandMore } from "react-icons/md";
 import { faqData } from "./about-data";
 
 export default function AboutFaq() {
   const containerRef = useRef<any>(null);
-  const faqMapper = useCallback((data: any, index: number) => {
-    return (
-      <div className="faq" key={index}>
-        <div
-          className="question"
-          onClick={() => {
-            // const target = container.children[index].classList
-            const container = containerRef.current;
-            console.log(container.children[index]);
-          }}
-        >
-          <p>{data.question}</p>
-          <MdExpandMore className="expand-svg" />
-          <MdExpandLess className="collapse-svg" />
+  const [isActive, setIsActive] = useState(false);
+  const faqMapper = useCallback(
+    (data: any, index: number) => {
+      return (
+        <div className="faq" key={index}>
+          <div
+            className="question"
+            onClick={() => {
+              // const target = container.children[index].classList
+              const container = containerRef.current;
+              setIsActive(true);
+              console.log(container.children[index], isActive);
+            }}
+          >
+            <p>{data.question}</p>
+            {isActive ? (
+              <MdExpandLess className="collapse-svg" />
+            ) : (
+              <MdExpandMore className="expand-svg" />
+            )}
+          </div>
+          <p className={`answer _${index}`}>{data.answer}</p>
         </div>
-        <p className={`answer _${index}`}>{data.answer}</p>
-      </div>
-    );
-  }, []);
+      );
+    },
+    [isActive]
+  );
   return (
     <div className="faq-wrapper">
       <h2 className="faq-heading">FAQ&apos;s</h2>
