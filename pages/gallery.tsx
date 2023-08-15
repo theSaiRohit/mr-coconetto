4200e643b1a2a4407d66a94612bf11072dffca66
Abhas Chatterjee <abhas2201@gmail.com>
2023-08-15 14:28:03 +0530

    gallery stuff

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,6 +1,18 @@
+import { GetStaticProps } from "next";
+import { GQLClient } from "@/clients";
+import { galleryItems } from "@/gql/queries";
 import Gallery from "@/modules/gallery";
-// import { GQLClient } from "@/clients";
+import { galImageData } from "@/modules/gallery/gallery-interface";
 
-export default function gallery() {
-  return <Gallery />;
+export const getStaticProps: GetStaticProps = async () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const galleryData: any = await GQLClient.request(galleryItems);
+  return {
+    props: { data: galleryData?.galleryCollection?.items },
+    revalidate: 6 * 60 * 60,
+  };
+};
+
+export default function gallery({ data }: { data: galImageData[] }) {
+  return <Gallery gdata={data ?? []} />;
 }
